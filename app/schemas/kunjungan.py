7e302441ea637ce6658eed6444c3754440c93dd0
Azwardi <Azwardi@gmail.com>
2026-05-11 11:54:12 +0700

    ke-4

--- app/schemas/kunjungan.py
+++ app/schemas/kunjungan.py
@@ -1,3 +1,4 @@
+from tokenize import String
 from typing import Optional
 
 from pydantic import BaseModel
@@ -7,4 +8,8 @@ class KunjunganCreate(BaseModel):
     idpasien:  Optional[int] = None
     keluhan: str
     tgl_kunjungan: date
-   
+    nama:  Optional[str] = None
+    alamat  :  Optional[str] = None
+    tgl_lahir: Optional[date] = None              
+    jenis_kelamin: Optional[str] = None              
+    no_hp: Optional[str] = None 
